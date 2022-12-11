a10bc75334227b075ee7abfacc5f84cb0a9297a6
Doe-P <phoukhando@gmail.com>
2022-12-11 11:21:22 +0700

    create challange set usertype and user has group in share preferrence and group list set space and home page

--- lib/apis/help_api.dart
+++ lib/apis/help_api.dart
@@ -8,3 +8,22 @@ String baseUrl = "http://157.245.158.229";
 
 // end point
 String endPoint = "$baseUrl/api";
+
+Future headers({bool isJson = false}) async {
+  final String _token = await SharePreferences.getAccessToken();
+  dynamic headers;
+  if (isJson) {
+    headers = {
+      HttpHeaders.authorizationHeader: "Bearer $_token",
+      HttpHeaders.acceptHeader: "application/json",
+      HttpHeaders.contentTypeHeader: "application/json",
+    };
+  } else {
+    headers = {
+      HttpHeaders.authorizationHeader: "Bearer $_token",
+      HttpHeaders.acceptHeader: "application/json",
+    //  HttpHeaders.contentTypeHeader: "application/json",
+    };
+  }
+  return headers;
+}
